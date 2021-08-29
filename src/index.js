2727545db009648ff22dc2135b731a50742f62e9
Your Name <nguenviktoria@gmail.com>
2021-08-30 00:10:19 +0300

    feat: add js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = ['|', '7', '8']
+  const open_brackets = ['(', '{', '[', '1', '3', '5'];
+  const brackets_pair = {
+    [')']: '(',
+    ['}']: '{',
+    [']']: '[',
+    ['2']: '1',
+    ['4']: '3',
+    ['6']: '5',
+  };
+
+  
+let stack = [];
+  for(let i = 0; i < str.length; i++) {
+    
+    let topElement = stack[stack.length - 1];
+    let currentSymbol = str[i];
+
+    if (open_brackets.includes(currentSymbol) ) {
+      stack.push(currentSymbol);
+    } else if (brackets.includes(currentSymbol) && stack.includes(currentSymbol)) {
+      stack.pop();
+    } else if (brackets.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else {
+      if  (brackets_pair[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
